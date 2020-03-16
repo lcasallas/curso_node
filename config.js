e7d1db70873a40090038a8539d4912bbdfb6ac1d
Leonardo Beltran <leonardo.casallas.beltran@gmail.com>
2020-03-15 21:35:20 -0500

    modificacion api

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const config = {
   dbUrl:
-    process.env.DB_URL ||
-    'mongodb+srv://db_user_lecode:PZxTfBizVWOrUpQ7@cluster0-neaoj.mongodb.net/foodplaner',
+    process.env.DB_URL || 'insertar url mongo'
   port: process.env.PORT || 4000,
   host: process.env.HOST || 'http://localhost',
   publicRoute: process.env.PUBLIC_ROUTE || '/app',
